f56700938edc2f458c4be1768b21ae5e543352e2
mahesh15698 <maheshjadhav7979@gmail.com>
2023-03-19 01:28:59 -0400

    Succefully added required folders with dundar init

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 #Declaring Variables for setup functions
 PROJECT_NAME= "hOUSING-Predictor"
-VERSION="0.0.1"
+VERSION="0.0.3"
 AUTHOR ="Mahesh Jadhav"
 DESCRIPTION ="This first Machine Leaning Deployment Project(Housing)"
-PACKAGES =["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
-
+HYPHEN_E_DOT = "-e ."
 def get_requirements_list()->List[str]:
     """
     Description: This function is going to return list of requirement
@@ -17,21 +16,19 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
-        # requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
-        # if HYPHEN_E_DOT in requirement_list:
-        #     requirement_list.remove(HYPHEN_E_DOT)
-        # return requirement_list
-    
+        requirement_list= requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
 
 
- 
 setup(
 name =PROJECT_NAME,
 version=VERSION, 
 author=AUTHOR,
 description=DESCRIPTION,
-packages= PACKAGES,
+packages= find_packages(),
 install_requires=get_requirements_list()
 
 )
