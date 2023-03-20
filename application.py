ae7ea62304f1347ce79ad7f2a0eedec00e51ce89
mahesh15698 <maheshjadhav7979@gmail.com>
2023-03-20 14:07:33 -0400

    Created a Logger and Exception file

--- application.py
+++ application.py
@@ -1,11 +1,19 @@
 from flask import Flask
-
+from Housing.logger import logging
+from Housing.exception import HousingException
+import sys
 application = Flask(__name__)
 app = application
 
 
 @app.route("/", methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("we are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
     return "Starting House Prediction Project"
 
 if __name__ == '__main__':
